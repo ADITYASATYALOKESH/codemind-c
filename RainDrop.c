e627b7ed9a412d895ba37c685692646139459371
ADITYASATYALOKESH <147477817+ADITYASATYALOKESH@users.noreply.github.com>
2023-10-26 14:23:08 +0530

    #include<stdio.h> int main() {     int x;     scanf("%d",&x);
        if(x%5==0&&x%3==0&&x%7==0)
        
        {
            printf("PlingPlangPlong");
        }
        else if(x%5==0&&x%3==0)
        {
            printf("PlingPlang");
        }
        else if(x%5==0&&x%7==0)
        {
            printf("PlangPlong");
        }
        else if(x%3==0&&x%7==0)
        {
            printf("plingplong");
        }
        else if(x%3==0)
        {
            printf("Pling");
        }
        else if(x%5==0)
        {
            printf("Plang");
        }
        else if(x%7==0)
        {
            printf("Plong");
        }
        else
        {
            printf("%d",x);
        }
    }

---
+++ RainDrop.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+int main()
+{
+    int x;
+    scanf("%d",&x);
+
+    if(x%5==0&&x%3==0&&x%7==0)
+    
+    {
+        printf("PlingPlangPlong");
+    }
+    else if(x%5==0&&x%3==0)
+    {
+        printf("PlingPlang");
+    }
+    else if(x%5==0&&x%7==0)
+    {
+        printf("PlangPlong");
+    }
+    else if(x%3==0&&x%7==0)
+    {
+        printf("plingplong");
+    }
+    else if(x%3==0)
+    {
+        printf("Pling");
+    }
+    else if(x%5==0)
+    {
+        printf("Plang");
+    }
+    else if(x%7==0)
+    {
+        printf("Plong");
+    }
+    else
+    {
+        printf("%d",x);
+    }
+}
