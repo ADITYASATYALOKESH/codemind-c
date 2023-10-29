2519bf241f6c6ded4b9686221afb7994a234aba4
ADITYASATYALOKESH <147477817+ADITYASATYALOKESH@users.noreply.github.com>
2023-10-29 14:27:11 +0530

    #include<stdio.h> int main() {     int x,y,z;     scanf("%d%d",&x,&y);     z=21-x-y;     if(z>=1&&z<=10)     {         printf("%d",z);     }     else     {         printf("-1");     }
    }

---
+++ Blackjack.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int x,y,z;
+    scanf("%d%d",&x,&y);
+    z=21-x-y;
+    if(z>=1&&z<=10)
+    {
+        printf("%d",z);
+    }
+    else
+    {
+        printf("-1");
+    }
+    
+}
